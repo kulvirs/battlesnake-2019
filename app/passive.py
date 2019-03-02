09e0a6301378dbbfad9e7754d9c9feaa58fe97f6
Kulvir Sekhon <kulvirsekhon11@hotmail.com>
2019-03-02 15:03:03 -0800

    Reverted back to passive

--- app/passive.py
+++ app/passive.py
@@ -3,6 +3,5 @@ from .grid import *
 # Returns true if snake should be passive.
 def passive_heuristic(board, health, head, food, board_dim, length, num_snakes):
     if num_snakes <= 2:
-        return False
-        #return health - board.manhattan_distance(head[0], head[1], food[0], food[1]) > 6*board_dim + 1 and length > 4
+        return health - board.manhattan_distance(head[0], head[1], food[0], food[1]) > 6*board_dim + 1 and length > 4
     return health - board.manhattan_distance(head[0], head[1], food[0], food[1]) > 6*board_dim + 1 and length > 4
